c5d3a879bab0925279abd27b095b511765e49367
berty <lucho.2012.tandil@gmail.com>
2022-02-07 20:31:17 -0300

    Ahora el menu de opciones de obra, recibe los metodos necesarios para realizar su funcionalidad

--- src/app/tab1/components/obras.list/obras.list.component.ts
+++ src/app/tab1/components/obras.list/obras.list.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AlertController, LoadingController } from '@ionic/angular';
+import { AlertController, LoadingController, ModalController } from '@ionic/angular';
 import { ApiConsumer } from 'src/app/models/ApiConsumer';
 import { ConfigService } from 'src/app/services/config.service';
 import { PrivateObrasService } from 'src/app/services/private.obras.service';
-import { Tab2Service } from 'src/app/tab2/tab2.service';
+
 import { Tab1Service } from '../../tab1.service';
+import { ObrasMenuComponent } from '../obras.menu/obras.menu.component';
 
 @Component({
   selector: 'app-obras-list',
@@ -21,10 +22,21 @@ export class ObrasListComponent  extends ApiConsumer  implements OnInit, OnDestr
     private privateObrasService:   PrivateObrasService,
     private router:                Router,
     private tab1Service:           Tab1Service,
-    private tab2Service:           Tab2Service,
-    public  configService:         ConfigService
+    public  configService:         ConfigService,
+    public modalController:        ModalController
   ) { 
     super(alertController, loadingController, ref);
+  }
+
+  async modal_menu(obra:any){
+    let modal = await this.modalController.create({
+      component: ObrasMenuComponent,
+      componentProps: {
+        'obra':  obra,
+        'modal': this.modalController
+      }
+    });
+    return await modal.present();
   }
 
   public listado_obras:any = [];
@@ -56,90 +68,6 @@ export class ObrasListComponent  extends ApiConsumer  implements OnInit, OnDestr
 
   consultar_todas(){
     this.loadingEspecificData(this.privateObrasService, 'expand=imagen',   'listado_obras', 'Consultando obras.');
-  }
-
-  nueva_nota(obra){
-    this.tab2Service.nueva_nota_obra_id = obra.id;
-    this.tab2Service.navigationOrigin = '/tabs/tab1';
-    this.router.navigate([ '/tabs/tab2/crear_nota' ]);
-  }
-
-  ver_notas(obra:any){
-    this.tab2Service.ver_nota_obra_id     = obra.id;
-    this.tab2Service.ver_nota_obra_nombre = obra.nombre_alias;
-    this.router.navigate([ '/tabs/tab2/notas' ]);
-  }
-
-  async eliminar_obra(obra:any){
-    const alert = await this.alertController.create({
-      header: 'Atención',
-      message: 'Está por eliminar la obra "' + obra.nombre_alias + '" y se perderán sus notas asociadas ¿desea continuar?.',
-      buttons: [{
-        text: 'No',
-        role: 'cancel',
-        cssClass: 'secondary',
-        handler: () => {}
-      }, {
-        text: 'Si',
-        cssClass: 'danger',
-        handler: () => {
-          this.borrar_obra(obra);
-        }
-      }]
-    });
-    await alert.present();
-  }
-
-  async borrar_obra(obra:any){
-    const loading = await this.loadingController.create({ message: 'Borrando obra: ' + obra.nombre_alias});
-    await loading.present();
-    this.privateObrasService.delete(obra.id).subscribe(
-      ok => {
-        loading.dismiss();
-        this.obra_filter_enabled = 'enabled';
-        this.tab1Service.recargarObras.next();
-      },
-      err => {
-        loading.dismiss();
-        this.displayAlert('Ocurrió un error al intentar eliminar la obra: ' + obra.nombre_alias);
-      }
-    );
-  }
-
-  async desabilitar_obra(obra:any){
-    obra.habilitada = 0;
-    const loading = await this.loadingController.create({ message: 'Desabilitando obra: ' + obra.nombre_alias});
-    await loading.present();
-
-    this.privateObrasService.put(obra,obra.id).subscribe(
-      ok => {
-        loading.dismiss();
-        this.obra_filter_enabled = 'enabled';
-        this.tab1Service.recargarObras.next();
-      },
-      err => {
-        loading.dismiss();
-        this.displayAlert('Ocurrió un error al intentar editar la obra: ' + obra.nombre_alias);
-      }
-    );
-  }
-
-  async habilitar_obra(obra:any){
-    obra.habilitada = 1;
-    const loading = await this.loadingController.create({ message: 'Habilitando obra: ' + obra.nombre_alias});
-    await loading.present();
-
-    this.privateObrasService.put(obra,obra.id).subscribe(
-      ok => {
-        loading.dismiss();
-        this.obra_filter_enabled = 'enabled';
-        this.tab1Service.recargarObras.next();
-      },
-      err => {
-        loading.dismiss();
-        this.displayAlert('Ocurrió un error al intentar editar la obra: ' + obra.nombre_alias);
-      }
-    );
   }
 
   ngOnDestroy(){
--- src/app/tab1/components/obras.menu/obras.menu.component.ts
+++ src/app/tab1/components/obras.menu/obras.menu.component.ts
@@ -1,6 +1,10 @@
-import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AlertController, LoadingController } from '@ionic/angular';
 import { ApiConsumer } from 'src/app/models/ApiConsumer';
+import { PrivateObrasService } from 'src/app/services/private.obras.service';
+import { Tab2Service } from 'src/app/tab2/tab2.service';
+import { Tab1Service } from '../../tab1.service';
 
 @Component({
   selector: 'app-obras.menu',
@@ -9,14 +13,118 @@ import { ApiConsumer } from 'src/app/models/ApiConsumer';
 })
 export class ObrasMenuComponent  extends ApiConsumer  implements OnInit, OnDestroy {
 
+  @Input() obra: string;
+  @Input() modal: any;
+
   constructor(
     private alertController:       AlertController,
     public  loadingController:     LoadingController,
     public  ref:                   ChangeDetectorRef,
+
+    private tab2Service:           Tab2Service,
+    private tab1Service:           Tab1Service,
+    private router:                Router,
+    
+    private privateObrasService:   PrivateObrasService,
   ) { 
     super(alertController, loadingController, ref);
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+
+  }
+
+  volver(){
+    this.modal.dismiss();
+  }
+
+  nueva_nota(obra){
+    this.tab2Service.nueva_nota_obra_id = obra.id;
+    this.tab2Service.navigationOrigin = '/tabs/tab1';
+    this.router.navigate([ '/tabs/tab2/crear_nota' ]);
+    this.volver();
+  }
+
+  ver_notas(obra:any){
+    this.tab2Service.ver_nota_obra_id     = obra.id;
+    this.tab2Service.ver_nota_obra_nombre = obra.nombre_alias;
+    this.router.navigate([ '/tabs/tab2/notas' ]);
+    this.volver();
+  }
+
+  async eliminar_obra(obra:any){
+    const alert = await this.alertController.create({
+      header: 'Atención',
+      message: 'Está por eliminar la obra "' + obra.nombre_alias + '" y se perderán sus notas asociadas ¿desea continuar?.',
+      buttons: [{
+        text: 'No',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: () => {}
+      }, {
+        text: 'Si',
+        cssClass: 'danger',
+        handler: () => {
+          this.borrar_obra(obra);
+        }
+      }]
+    });
+    await alert.present();
+  }
+
+  async borrar_obra(obra:any){
+    const loading = await this.loadingController.create({ message: 'Borrando obra: ' + obra.nombre_alias});
+    await loading.present();
+    this.privateObrasService.delete(obra.id).subscribe(
+      ok => {
+        loading.dismiss();
+        //this.obra_filter_enabled = 'enabled';
+        this.tab1Service.recargarObras.next();
+      },
+      err => {
+        loading.dismiss();
+        this.displayAlert('Ocurrió un error al intentar eliminar la obra: ' + obra.nombre_alias);
+      }
+    );
+    this.volver();
+  }
+
+  async desabilitar_obra(obra:any){
+    obra.habilitada = 0;
+    const loading = await this.loadingController.create({ message: 'Desabilitando obra: ' + obra.nombre_alias});
+    await loading.present();
+
+    this.privateObrasService.put(obra,obra.id).subscribe(
+      ok => {
+        loading.dismiss();
+        //this.obra_filter_enabled = 'enabled';
+        this.tab1Service.recargarObras.next();
+      },
+      err => {
+        loading.dismiss();
+        this.displayAlert('Ocurrió un error al intentar editar la obra: ' + obra.nombre_alias);
+      }
+    );
+    this.volver();
+  }
+
+  async habilitar_obra(obra:any){
+    obra.habilitada = 1;
+    const loading = await this.loadingController.create({ message: 'Habilitando obra: ' + obra.nombre_alias});
+    await loading.present();
+
+    this.privateObrasService.put(obra,obra.id).subscribe(
+      ok => {
+        loading.dismiss();
+        //this.obra_filter_enabled = 'enabled';
+        this.tab1Service.recargarObras.next();
+      },
+      err => {
+        loading.dismiss();
+        this.displayAlert('Ocurrió un error al intentar editar la obra: ' + obra.nombre_alias);
+      }
+    );
+    this.volver();
+  }
 
 }
